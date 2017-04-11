e56817d4fbdf417d95b6582cf950134848be9f21
jmm <Jean-Marc.Molines@univ-grenoble-alpes.fr>
2017-04-11 21:37:58 +0200

    delete matlab code before pushing to github

--- TIME_SERIES/matlab/SINGLE/NotUsed/range_year.m
+++
@@ -1,9 +0,0 @@
-function [starty endy]=range_year(filename,nhead)
-%%%%%%%%%%%%%%%%%%%%%%%
-%%%%%%%%%%%%%%%%%%%%%%% PLOTTING ROUTINE FOR TRANSPORTS
-%%%%%%%%%%%%%%%%%%%%%%%
-eval(['tmp=load(''' filename ''');']) ;
-starty  = tmp(nhead+1  ,1) ; 
-endy    = tmp(end,1) ;
-clear tmp
-
